0e543441b79814c608313d128b955a8b8ba11344
nely de faria <nelly.nf54@gmail.com>
2021-02-28 20:13:05 -0300

    Adiciona arquivo do-while.js

---
+++ src/do-while.js
@@ -0,0 +1,16 @@
+let numeroEscolhido = Math.floor((Math.random() * 9) + 1),
+    acertou = false,
+    palpite = '';
+
+do {
+    palpite = prompt('Qual o seu número?', 0);
+    if (parseInt(palpite) === numeroEscolhido) {
+        acertou = true;
+    }
+} while (palpite !== '' && parseInt(palpite) !== numeroEscolhido);
+
+if (acertou) {
+    console.log('Vitória');
+} else {
+    console.log('Não foi dessa vez');
+}
