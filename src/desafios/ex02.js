fab83ca8d1e0412aedcad9ab450b9a7b5083ea7d
nely de faria <nelly.nf54@gmail.com>
2021-03-01 19:36:37 -0300

    Adiciona ex02.js

---
+++ src/desafios/ex02.js
@@ -0,0 +1,17 @@
+function adicao(numero1, numero2){
+    //Informe seu código de adição
+    console.log(numero1 + numero2);
+    
+}
+function subtracao(numero1, numero2){
+    //Informe seu código de subtração
+    console.log(numero1 - numero2);
+}
+function multiplicacao(numero1, numero2){
+    //Informe seu código de multiplicação
+    console.log(numero1 * numero2);
+}
+function divisao(numero1, numero2){
+    //Informe seu código de divisão
+    console.log(numero1 / numero2);
+}
