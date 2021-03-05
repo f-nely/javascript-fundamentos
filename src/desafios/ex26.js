7c2386babccac02c0da43a7a8a767c20257957d6
nely de faria <nelly.nf54@gmail.com>
2021-03-05 18:49:04 -0300

    Adiciona ex26.js

---
+++ src/desafios/ex26.js
@@ -0,0 +1,8 @@
+function main(palavra) {
+    
+    let palavraInvertida = '';
+    for(let i = palavra.length - 1; i >= 0; i--) {
+        palavraInvertida += palavra[i];
+    }
+    console.log(palavraInvertida);
+}
